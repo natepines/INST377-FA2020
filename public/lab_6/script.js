aa20a98d23d6784603e7dde111dfeca2c83064d0
natepines <natelpines@verizon.net>
2020-10-24 17:49:56 -0400

    lab 6

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -1,5 +1,6 @@
 // You may wish to find an effective randomizer function on MDN.
 
+
 function range(int) {
   const arr = [];
   for (let i = 0; i < int; i += 1) {
@@ -28,7 +29,7 @@ document.body.addEventListener('submit', async (e) => {
     body: JSON.stringify(form)
   })
     .then((fromServer) => fromServer.json())
-    .then((fromServer) => {
+    .then((fromServer) => { 
       // You're going to do your lab work in here. Replace this comment.
       console.log('fromServer', fromServer);
     })
